be5baf9be04f585b2ea6554d52b1b0fbf7e1973c
Dan F-M <dan@danfm.ca>
2011-09-07 16:32:32 -0400

    initial skeleton

---
+++ gp/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Gaussian processes module
+
+History
+-------
+2011-09-07 - Created by Dan Foreman-Mackey
+
+"""
+
+
---
+++ gp/gp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+My Gaussian process object
+
+History
+-------
+2011-09-07 - Created by Dan Foreman-Mackey
+
+"""
+
+__all__ = ['GaussianProcess']
+
+import numpy as np
+
+class GaussianProcess(object):
+    """
+    A Gaussian process object
+
+    History
+    -------
+    2011-09-07 - Created by Dan Foreman-Mackey
+
+    """
+    def __init__(self):
+        pass
+
