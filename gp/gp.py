537481bf20f5d270ad400ea5ead2426e7634c20d
Dan F-M <dan@danfm.ca>
2011-09-23 11:22:23 -0400

    install script, optimization, badass, etc.

--- gp/gp.py
+++ gp/gp.py
@@ -14,10 +14,10 @@ from __future__ import division
 __all__ = ['GaussianProcess']
 
 import numpy as np
-np.random.seed(2)
 import scipy.sparse as sp
 import scipy.sparse.linalg
-import time
+from scipy.linalg import det
+import scipy.optimize as op
 
 class GaussianProcess(object):
     """
@@ -43,46 +43,76 @@ class GaussianProcess(object):
         return "GaussianProcess(s2=%f,a=%f,l2=%f)"%(self._s2,self._a,self._l2)
 
     def k(self,x1,x2,chi2max=25.0):
+        """
+        The default kernel function
+
+        Parameters
+        ----------
+        x1,x2 : numpy.ndarray
+            Vectors of positions.
+
+        chi2max : float, optional
+            Set clipping for sparseness.
+
+        Returns
+        -------
+        k : numpy.ndarray
+            Covariance matrix between x1 and x2.
+
+        Note
+        ----
+        This works well for small matrices but it is poorly implemented for larger
+        matrices --- especially if they are actually sparse!
+
+        """
         d = (x1-x2)**2/self._l2
+        k = sp.lil_matrix(d.shape)
         k = self._a*np.exp(-0.5*d)
         k[d > chi2max] = 0.0
-        return k
+        return sp.lil_matrix(k).tocsc()
 
     def K(self,x,y=None):
-        b = self.k(x[:,np.newaxis],x[np.newaxis,:]) \
-            + self._s2*np.identity(len(x))
+        if y is None:
+            y = x
+        b = self.k(x[:,np.newaxis],y[np.newaxis,:]) \
+            + self._s2*sp.identity(len(x),format="csc")
         return b
 
     def fit(self,x,y):
-        Kxx = self.K(x,x)
-        nnz = sp.lil_matrix(Kxx).nnz
-        print "NNZ: ",nnz,nnz/np.product(np.shape(Kxx))
+        self._x = x
+        self._y = y
+        self._Kxx = self.K(x)
+        self._detK = None
+        self._L = sp.linalg.splu(self._Kxx)
+        self._alpha = self._L.solve(y)
 
-        strt = time.time()
-        self._L = sp.linalg.factorized(sp.lil_matrix(Kxx).tocsc())
-        self._alpha = self._L(y)
-        print "sparse: ", time.time()-strt
+    def __call__(self,x,cov=False):
+        assert self._L is not None
+        ks = self.k(x[:,np.newaxis],self._x[np.newaxis,:])
+        # calculate the mean
+        f = ks.dot(np.atleast_2d(self._alpha).T)[:,0]
+        if not cov:
+            return f
+        kss = self.k(x[:,np.newaxis],x[np.newaxis,:])
+        kik = np.zeros(ks.shape)
+        for i in xrange(ks.shape[0]):
+            kik[i,:] = self._L.solve(np.array(ks[i,:].todense())[0])
+        v = kss - ks.dot(kik.T)
+        return f,v
 
-    def test_sparse(self):
-        N = 8000
-        x = 1000*np.random.rand(N)
-        y = np.sin(x) + 0.1*np.random.randn(N)
+    def optimize(self,x,y):
+        def chi2(p):
+            print p
+            self._a,self._l2,self._s2 = p**2
+            self.fit(x,y)
+            detK = np.log(det(self._Kxx.todense())) \
+                     + len(y)*np.log(2*np.pi)
+            c2 = np.dot(y,self._alpha) + detK
+            return c2
 
-        # do it using dense algebra
-        # Kxx = self.K(x,x)
-        # strt = time.time()
-        # alpha = np.linalg.solve(Kxx,y)
-        # print "dense: ", time.time()-strt
-
-        # sparse algebra
-        self.fit(x,y)
-
-        print np.linalg.norm(alpha-self._alpha)
-        assert np.linalg.norm(alpha-self._alpha) < 1e-10
-
-    def test_noiseless(self):
-        self._s2 = 0.01
-        self.test_sparse()
+        p0 = np.sqrt([self._a,self._l2,self._s2])
+        p1 = op.fmin(chi2,p0)
+        print p1,(self._a,self._l2,self._s2)
 
     def sample_prior(self,x):
         """
@@ -105,45 +135,46 @@ class GaussianProcess(object):
         """
         return np.random.multivariate_normal(np.zeros(len(x)),self.K(x).todense())
 
-    def sample(self,x):
+    def sample(self,x,N=1):
         """
         Sample given some data
-
-        Note
-        ----
-        This assumes that K is symmetric for now...
 
         History
         -------
         2011-09-07 - Created by Dan Foreman-Mackey
 
         """
-        X = self._data[:,0]
-        f = self._data[:,1]
-        Kxx = self.full_kernel(X,X)
-        invKxx = np.linalg.inv(Kxx)
-        Kxs = self.full_kernel(X,x)
-        Ksx = Kxs.T
-        Kss = self.full_kernel(x,x)
-        mean = self._mean(x)+np.dot(Ksx,np.dot(invKxx,f))
-        cov = Kss - np.dot(Ksx,np.dot(invKxx,Kxs))
-        return np.random.multivariate_normal(mean,cov)
+        mean,cov = self(x,cov=True)
+        return np.random.multivariate_normal(mean,cov,N)
 
 if __name__ == '__main__':
-    import sys
     import pylab as pl
+    np.random.seed(5)
 
-    x = np.random.rand(5)
-    y = np.random.rand(5)
+    N = 50
+    s = 10
+    x = s*np.random.rand(N)
+    y = np.sin(x) + 0.1*np.random.randn(N)
 
-    p = GaussianProcess()
-    p.test_noiseless()
+    p = GaussianProcess(l2=0.5)
+    p._s2 = 0.001
+    p.optimize(x,y)
 
-    sys.exit(0)
-    for i in range(100):
-        x = np.random.rand(100)*10-5
-        y = p.sample_prior(x)
-        pl.plot(x,y,'.k',alpha=0.3)
-    pl.plot(data[:,0],data[:,1],'or')
-    pl.show()
+    # plot fit
+    x0 = np.linspace(min(x),max(x),500)
+    y0 = p.sample(x0,100)
+    pl.plot(x0,y0.T,'.k',alpha=0.1)
+
+    mean,cov = p(x0,cov=True)
+    pl.plot(x0,mean)
+
+    #plot data
+    pl.plot(x,y,'.r')
+
+    pl.savefig("%d.png"%s)
+
+    pl.figure()
+    pl.imshow(cov)
+
+    pl.savefig("cov-%d.png"%s)
 
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(name='dfmgp',
+        version='0',
+        description='Gaussian Processes',
+        author='Daniel Foreman-Mackey',
+        author_email='danfm@nyu.edu',
+        packages=['gp'],
+    )
+
